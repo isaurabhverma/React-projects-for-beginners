3e7a14e07705897c3755b91c3e5f105b5b920423
meetarora <meet.arora2403@gmail.com>
2025-10-05 13:44:45 +0530

    added random-advice-generator

---
+++ Random-Advice-Generator/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Advice from './Advice'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <Advice />
+    </div>
+  )
+}
+
+export default App
